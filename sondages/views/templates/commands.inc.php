179d3d73c59be9632765b8b652e68ee609084fc8
lswart <lauren.swart@gmail.com>
2021-10-22 18:23:06 +0200

    Project initialisation

---
+++ sondages/views/templates/commands.inc.php
@@ -0,0 +1,10 @@
+
+<a href="<?php echo $_SERVER['PHP_SELF'].'?action=AddSurveyForm' ?>">Poster
+	un sondage</a>
+|
+<a href="<?php echo $_SERVER['PHP_SELF'].'?action=GetMySurveys' ?>">Afficher
+	mes sondages</a>
+|
+<a href="<?php echo $_SERVER['PHP_SELF'].'?action=GetMySurveys' ?>">Changer 
+	mon mot de passe</a>
+
