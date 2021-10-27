097dd5601e8f299dad983b8c41f73c0ad4d49ace
lswart <lauren.swart@gmail.com>
2021-10-27 18:08:04 +0200

    eviter l'attaque xss au niveau du nickname

--- sondages/views/templates/updateuserform.inc.php
+++ sondages/views/templates/updateuserform.inc.php
@@ -6,7 +6,7 @@ if ($model->getMessage()!=="") {
 <br />
 
 <form class="form" method="post" action="index.php?action=UpdateUser" >
-				<label> Pseudo : </label><div class="field fixed"><?php echo $model->getLogin(); ?></div><br>
+				<label> Pseudo : </label><div class="field fixed"><?php echo htmlentities($model->getLogin()); ?></div><br>
 				<label for="updatePassword"> Mot de passe : </label>
 				<input class="field" name="updatePassword" type="password" /><br>
 				<label for="updatePassword2">Confirmation : </label>
