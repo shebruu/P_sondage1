e87bb2e13e326091348a0efb4060a96aaf51a2c8
lswart <lauren.swart@gmail.com>
2021-11-05 15:51:51 +0100

    limit percentage display to 2 decimals

--- sondages/views/templates/survey.inc.php
+++ sondages/views/templates/survey.inc.php
@@ -8,8 +8,8 @@
 <?php foreach ($survey->getResponses() as $response) { ?>
     <div class="response">
     	<span class="responseTitle"><?= $response->getTitle() ?></span>
-    	<span><progress max="100" value="<?= $response->getPercentage() ?>" class="chart"></progress></span>
-    	<span class="count"><?= $response->getPercentage() ?> %</span>
+    	<span><progress max="100" value="<?= number_format($response->getPercentage(), 2) ?>" class="chart"></progress></span>
+    	<span class="count"><?= number_format($response->getPercentage(), 2)  ?> %</span>
     	<form class="voteForm" method="post" action="index.php?action=Vote">
     		<input type="text" hidden name="responseId" value="<?= $response->getId() ?>" >
     		<button class="submit">Voter</button>
